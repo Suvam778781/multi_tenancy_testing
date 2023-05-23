22b1bf2715161a0d073090d1468a2aac7b19306b
manmohanjina <manish63singh@gmail.com>
2023-05-24 00:02:29 +0530

    quick checkc

--- index.js
+++ index.js
@@ -13,11 +13,7 @@ const { userTodoRoute } = require("./Routes/todoRoute");
 const { sendEmail } = require("./middleware/email&pass.sender");
 app.use(express.json())
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
