376c7548e9605dc6c66f49790d60598e1a010a46
manmohanjina <manish63singh@gmail.com>
2023-05-24 09:57:43 +0530

    cors error fix

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const { sendEmail } = require("./middleware/email&pass.sender");
 app.use(express.json())
 
 
+app.use(cors())
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
