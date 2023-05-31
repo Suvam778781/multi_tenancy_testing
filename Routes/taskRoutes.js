5084a5ed2103d239e64c8c0f711cab480eaa6537
Suvam Swagatam panda <107461587+Suvam778781@users.noreply.github.com>
2023-05-31 10:31:49 +0530

    update task and subtask routes

---
+++ Routes/taskRoutes.js
@@ -0,0 +1,12 @@
+const express=require("express");
+const { handleGetTasks, handleDeleteSubtask, handleUpdateSubtask, handleDeleteTask, handleUpdateTask, handelAddSubTask } = require("../RouterController/taskController");
+const taskRouter=express.Router();
+
+
+taskRouter.get("/alltask/:userId",handleGetTasks)
+taskRouter.post('/addsubtask/:id',handelAddSubTask)
+taskRouter.delete("/deletesubtask/:taskId/:maintaskId",handleDeleteSubtask)
+taskRouter.patch("/updatesubtask/:taskId/:maintaskId",handleUpdateSubtask)
+taskRouter.delete("/deletetask/:userId/:taskId",handleDeleteTask)
+
+module.exports={taskRouter}
